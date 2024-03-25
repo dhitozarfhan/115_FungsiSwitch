dfb2519604d7020d7ba84200e5ebb0fbcd5ceead
dhitozarfhan <dhitozarfhan@gmail.com>
2024-03-25 10:46:45 +0700

    membuat prosedur input

--- praktek6_prosedur/praktek6_prosedur.cpp
+++ praktek6_prosedur/praktek6_prosedur.cpp
@@ -28,3 +28,54 @@ int pembagian(int bil_1, int bil_2) {
 	return bil_1 + bil_2;
 }
 
+int main()
+{
+    
+    int pilihan;
+
+    do {
+        system("cls");
+        cout << "===== Menu =====" << endl;
+        cout << "1. Penjumlahan " << endl;
+        cout << "2. Pengurangan " << endl;
+        cout << "3. Perkalian " << endl;
+        cout << "4. Pembagian " << endl;
+        cout << "5. Keluar " << endl;
+        cout << "Masukkan menu yang ingin di pilih (1/2/3/4/5): ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+
+        case 1:
+            input();
+            cout << "Hasil penjumlahan = " << penjumlahan(a, b) << endl;
+            system("pause");
+            break;
+        case 2:
+            input();
+            cout << "Hasil pengurangan = " << pengurangan(a, b) << endl;
+            system("pause");
+            break;
+        case 3:
+            input();
+            cout << "Hasil perkalian = " << perkalian(a, b) << endl;
+            system("pause");
+            break;
+        case 4:
+            input();
+            cout << "Hasil pembagian = " << pembagian(a, b) << endl;
+            system("pause");
+            break;
+        case 5:
+            break;
+        default:
+            cout << "Menu tidak tersedia silahkan pilih lagi!!! " << endl;
+            break;
+
+
+        }
+    } while (pilihan != 5);
+
+}
+    
